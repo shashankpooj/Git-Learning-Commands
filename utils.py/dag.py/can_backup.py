ae726c496d4d0890a30f55ac029c99299d5e2e19
Shashank <shashank.p@simpleenergy.in>
2026-02-26 16:21:07 +0530

    chnage

---
+++ utils.py/dag.py/can_backup.py
@@ -0,0 +1,26 @@
+from airflow import DAG
+from airflow.operators.python import PythonOperator
+from datetime import datetime
+
+# --------------------------------
+# Python function (task logic)
+# --------------------------------
+def hello_task():
+    print("Hello! This is my first DAG.")
+
+# --------------------------------
+# DAG definition
+# --------------------------------
+with DAG(
+    dag_id="simple_hello_dag",
+    start_date=datetime(2024, 1, 1),
+    schedule_interval="@daily",
+    catchup=False
+) as dag:
+
+    task_hello = PythonOperator(
+        task_id="hello_task",
+        python_callable=hello_task
+    )
+
+    task1>>task2
